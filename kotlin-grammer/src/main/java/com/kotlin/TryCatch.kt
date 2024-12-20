398efb0c44671a0a983e4ae224304d0a34a62ded
jangdm37 <jangdm37@gmail.com>
2024-12-20 09:12:59 +0900

    kotlin exception

---
+++ kotlin-grammer/src/main/java/com/kotlin/TryCatch.kt
@@ -0,0 +1,22 @@
+package com.kotlin
+
+import javax.imageio.IIOException
+
+fun main() {
+    val str = "1"
+
+    if (str.length > 10) {
+        throw IllegalStateException("str$str is too long")
+    }
+
+    parse(str)
+}
+
+fun parse(numberStr: String): Int = try {
+    Integer.parseInt(numberStr)
+} catch (e: Exception) {
+    throw IIOException("일부러 발생시키는 checked exception")
+} finally {
+    println("무조건 실행되는 코드 블록")
+}
+
